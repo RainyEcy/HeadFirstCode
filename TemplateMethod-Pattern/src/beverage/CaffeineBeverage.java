b6d1cb26e7504fe4bfd2e4fd6f67eaf320db019f
Rainy.ecy <shinerainy_ycy@163.com>
2021-01-30 20:43:01 +0800

    init templateMethod beverage

---
+++ TemplateMethod-Pattern/src/beverage/CaffeineBeverage.java
@@ -0,0 +1,24 @@
+package beverage;
+
+public abstract class CaffeineBeverage {
+
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    abstract void brew();
+
+    void pourInCup() {
+        System.out.println("Pouring in cup");
+    }
+
+    abstract void addCondiments();
+
+}
